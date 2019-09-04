c7f781770c51d78822b7ecc9bf16017517b6bba3
Bernardo Hernandez <bhernandez@consistent.com.mx>
2019-09-03 22:03:42 -0500

    Initial commit

---
+++ src/main/java/com/consistent/cuervo/constants/ServiceListaPreciosPortletKeys.java
@@ -0,0 +1,10 @@
+package com.consistent.cuervo.constants;
+
+/**
+ * @author bernardohernadez
+ */
+public class ServiceListaPreciosPortletKeys {
+
+	public static final String ServiceListaPrecios = "servicelistaprecios";
+
+}
---
+++ src/main/resources/META-INF/resources/view.jsp
@@ -0,0 +1,5 @@
+<%@ include file="/init.jsp" %>
+
+<p>
+	<b><liferay-ui:message key="servicelistaprecios.caption"/></b>
+</p>
